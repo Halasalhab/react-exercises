a8803cb2b88666a0d24e34487d6d9643445f8f39
halasalhab <halasalhab0@gmail.com>
2023-09-05 12:36:53 +0200

    setup files

---
+++ ex1/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+const appContainer = styled.div`
+
+`
+function App() {
+  return (
+    <div className="App">
+     <p><strong>Add members to Front-end development team</strong></p>
+    </div>
+  );
+}
+
+export default App;
