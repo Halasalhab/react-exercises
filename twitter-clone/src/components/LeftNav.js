41523d6d92d98198b31c0b5b9b1d3c0d44f3907a
halasalhab <halasalhab0@gmail.com>
2023-09-20 13:58:17 +0300

    added leftnav style

--- twitter-clone/src/components/LeftNav.js
+++ twitter-clone/src/components/LeftNav.js
@@ -9,33 +9,31 @@ function LeftNav() {
 			<Link>
 				<img src={favIcon} alt="logo" style={{ width: "40px" }} />
 			</Link>
-			<Link to="/">
+			<Link>
 				<h2>
 					<i className="fa fa-home" aria-hidden="true" /> <span className="title">Home</span>
 				</h2>
 			</Link>
-			<Link to="/profile">
+			<Link>
 				<h2>
 					<i className="fa fa-user" aria-hidden="true" /> <span className="title">Profile</span>
 				</h2>
 			</Link>
-			<Link to="/users">
+			<Link>
 				<h2>
 					<i className="fa fa-envelope" aria-hidden="true" /> <span className="title">Messages</span>
 				</h2>
 			</Link>
-			<Link to="/users">
+			<Link>
 				<h2>
 					<i className="fa fa-bell" aria-hidden="true" /> <span className="title">Notifications</span>
 				</h2>
 			</Link>
-			<Link to="/users">
+			<Link>
 				<h2>
 					<i className="fa fa-ellipsis-h" aria-hidden="true" /> <span className="title">More</span>
 				</h2>
 			</Link>
-			{/* <Tweet />
-			<Logout /> */}
 		</div>
 	)
 }
