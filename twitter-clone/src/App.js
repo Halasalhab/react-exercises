5e9ccdfaadf2956d8870f8a5954d6f99093addbe
halasalhab <halasalhab0@gmail.com>
2023-09-20 13:23:23 +0300

    added twiter logo & left nav structure

---
+++ twitter-clone/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Profile from './pages/profile/profile';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Profile />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
